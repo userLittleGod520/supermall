26d7338b6d1c56807f3a5a2a4fe4d3c35eac2f36
lishen <2070489981@qq.com>
2022-06-15 09:41:06 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import './assets/css/normaliz e.css'
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
